70534aafc2e0517d6098f86d263eb536ce7c469a
Oleksii Honchar <alekseyhonchar@gmail.com>
2023-01-02 16:36:17 +0000

    feat: done basic auth

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import User from './models/user.model.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI);
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/register', (req, res) => {
+  bcrypt.hash(req.body.password, 10, (error, hashedPassword) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      const user = new User({
+        email: req.body.email,
+        password: hashedPassword
+      });
+
+      user.save((error) => {
+        if (error) {
+          res.status(500).send(error);
+        } else {
+          res.send('User successfully registered');
+        }
+      });
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }, (error, user) => {
+    if (error) {
+      res.status(500).send(error);
+    } else if (!user) {
+      res.status(404).send('Email or password are incorrect');
+    } else {
+      bcrypt.compare(req.body.password, user.password, (error, result) => {
+        if (error) {
+          res.status(500).send(error);
+        } else if (!result) {
+          res.status(401).send('Email or password are incorrect');
+        } else {
+          const token = jwt.sign({ id: user._id }, 'secretkey');
+
+          res.send({ token });
+        }
+      });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
