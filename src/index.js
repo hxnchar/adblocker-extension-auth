80f8c699694541eb2834329ea49a82ad5c6f8b8f
Oleksii Honchar <alekseyhonchar@gmail.com>
2023-01-03 10:18:28 +0000

    feat: done auth

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
 import User from './models/user.model.js';
+import { genTokenPair } from './utils/token.util.js';
 
 dotenv.config();
 
@@ -14,6 +15,28 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  const path = req.url;
+  console.log(path);
+  if (path === '/register' || path === '/login') {
+     return next();
+  }
+  const token = req.headers['x-access-token'];
+  // Check if the token is present
+  if (!token) {
+    return res.status(401).send('Access token required');
+  }
+  // Verify the token
+  jwt.verify(token, 'secretkey', (error, decoded) => {
+    if (error) {
+      return res.status(401).send('Invalid access token');
+    }
+    // Set the user ID in the request object
+    req.userId = decoded.id;
+    // Call the next middleware function
+    next();
+  });
+});
 
 app.post('/register', async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -22,9 +45,9 @@ app.post('/register', async (req, res) => {
     password: hashedPassword,
   });
   try {
+    const tokens = await genTokenPair(user);
     await user.save();
-    const token = jwt.sign({ id: user._id }, 'secretkey');
-    return res.send({ token });
+    return res.send();
   } catch (e) {
     return res.status(500).send(e.message);
   }
@@ -36,20 +59,41 @@ app.post('/login', async (req, res) => {
     if (!user) {
       return res.status(404).send('User not found');
     }
-    bcrypt.compare(req.body.password, user.password, (error, result) => {
+    bcrypt.compare(req.body.password, user.password, async (error, result) => {
       if (error) {
         return res.status(500).send(error);
       }
       if (!result) {
         return res.status(401).send('Email or password are incorrect');
       }
-
-      const token = jwt.sign({ id: user._id }, 'secretkey');
-      return res.send({ token });
+      return res.send(await genTokenPair(user));
     });
   } catch (e) {
     return res.status(500).send(error);
   }
+});
+
+app.get('/refresh-token', async (req, res) => {
+  try {
+    const user = await User.findOne({refreshTokens: req.body.refreshToken});
+    if (!user) {
+      return res.status(401).send('Invalid refresh token');
+    }
+    return res.send(await genTokenPair(user));
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
+
+app.get('/protected', (req, res) => {
+  // The user ID is available on the request object
+  User.findById(req.userId, (error, user) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(`Welcome, ${user.email}!`);
+    }
+  });
 });
 
 app.listen(PORT, () => {
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const User = mongoose.model('User', {
   email: { 'type': String, 'unique': true, 'required': true },
   password: { 'type': String, 'unique': false, 'required': true },
+  refreshTokens: [{ 'type': String, 'unique': true }],
 });
 
 export default User;
